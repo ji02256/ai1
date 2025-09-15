ffee38f8507f2f6c4726c3c49c12e14222acacfc
신지섭 <js932@example.com>
2025-09-15 11:09:22 +0900

    1

---
+++ Project1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int row = 0;
+    int col;
+
+    while (row < 5) {
+        col = 0;
+        while (col < 5) {
+            // 테두리 위치일 때만 '*'
+            if (row == 0 || row == 4 || col == 0 || col == 4)
+                printf("*");
+            else
+                printf(" ");
+            col++;
+        }
+        printf("\n");
+        row++;
+    }
+
+    return 0;
+}
+
